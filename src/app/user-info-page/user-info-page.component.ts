021a9d9f4d591678066c7da2920482b25e2c2a01
KeybiW <intelligence1outof6@gmail.com>
2021-07-14 14:33:50 +0300

    v1.2

--- src/app/user-info-page/user-info-page.component.ts
+++ src/app/user-info-page/user-info-page.component.ts
@@ -1,29 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CardReaderComponent } from '../card-reader/card-reader.component';
 import { UserService } from '../user.service';
 import { Location } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+import { ActivatedRoute, Router } from '@angular/router';
+import { timeout } from 'q';
 
+let timer;
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
   styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent implements OnInit {
-
+export class UserInfoPageComponent implements OnInit, OnDestroy {
   constructor(
+    private route: ActivatedRoute,
     public user: UserService,
-    private location: Location
-
+    private location: Location,
+    private router: Router,
     ) {
-    setTimeout(() => {
-      // this.location.back(); // edit sayfasına dönebiliyor
-      window.location.replace('http://localhost:4200');
-    }, 60000);
-   }
+    timer = setTimeout(() => {
+      // this.location.back();
+      // window.location.replace('http://localhost:4200');
+      router.navigate(['/']);
+    }, 6000);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(timer);
+  }
+
 
   ngOnInit(): void {
+
   }
 
 
 }
+
+
 
